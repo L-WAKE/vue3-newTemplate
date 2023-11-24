0486e92408aed7c2558bf845881ebede926cbd1b
liujin <2454243839@qq.com>
2023-11-24 16:21:52 +0800

    国际化测试

---
+++ src/views/transition/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>登录页</p>
+    <br />
+    <span>测试</span>
+    <br />
+    <button @click="testFun">点击</button>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+const templateDatas = reactive({
+  testOne: "这是一个测试数据",
+  testTwo: "001",
+});
+const testFun = function () {
+  console.log("测试输出一个数据", templateDatas.testTwo);
+};
+</script>
